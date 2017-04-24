7ff333945937c9802215eeddeb554eba90b34404
Rebeca <rivke.5rabzatum@gmail.com>
2017-04-23 18:39:37 -0700

    miembro

--- payroll-base-proyecto/src/main/java/payrollcasestudy/transactions/change/ChangeMemberTransaction.java
+++ payroll-base-proyecto/src/main/java/payrollcasestudy/transactions/change/ChangeMemberTransaction.java
@@ -1,17 +1,28 @@
 package payrollcasestudy.transactions.change;
+import payrollcasestudy.boundaries.PayrollDatabase;
 import payrollcasestudy.entities.Employee;
 
-public class ChangeMemberTransaction  {
+public class ChangeMemberTransaction  extends changeAffiliationTransactionn {
 
 	private int memberId;
 	private double weeklyUnionDues;
 	
 	public ChangeMemberTransaction(int employeeId, int memberId, double weeklyUnionDues) {
 		// TODO Auto-generated constructor stub
-		
+		super(employeeId);
 		this.memberId=memberId;
 		this.weeklyUnionDues=weeklyUnionDues;
+		
 	}
+
+	@Override
+	protected void registerAsAMember(Employee employee) {
+		// TODO Auto-generated method stub
+		PayrollDatabase.globalPayrollDatabase.addUnionMember(memberId, employee);
+		
+	}
+
+	
 	
 
 }
